a0857c1eb75a1e9751d73b49184cba96685aa9a8
Kyra Buchmann <kac580@outlook.com>
2020-01-31 10:28:06 -0600

    updated project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GenresComponent } from './genres/genres.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { AlbumsComponent } from './albums/albums.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'genres', component: GenresComponent},
+  {path: 'artists', component: ArtistsComponent},
+  {path: 'albums', component: AlbumsComponent},
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenresComponent } from './genres/genres.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { AlbumsComponent } from './albums/albums.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenresComponent,
+    ArtistsComponent,
+    AlbumsComponent
   ],
   imports: [
     BrowserModule,
